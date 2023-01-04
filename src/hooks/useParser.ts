a0695d7ff5b1ac503162f0b28d3c3407dc6c09b3
yotarotsukada <yotarotsukada@gmail.com>
2023-01-04 17:21:40 +0900

    feature: KaTeXをサポート

--- src/hooks/useParser.ts
+++ src/hooks/useParser.ts
@@ -1,11 +1,13 @@
 import { Fragment, ReactElement, createElement } from 'react';
 import rehypeHighlight from 'rehype-highlight';
+import rehypeKatex from 'rehype-katex';
 import rehypeMinifyWhitespace from 'rehype-minify-whitespace';
 import rehypeParse from 'rehype-parse';
 import rehypeRaw from 'rehype-raw';
 import rehypeReact from 'rehype-react';
 import rehypeStringify from 'rehype-stringify';
 import remarkGfm from 'remark-gfm';
+import remarkMath from 'remark-math';
 import remarkParse from 'remark-parse';
 import remarkRehype from 'remark-rehype';
 import { unified } from 'unified';
@@ -14,15 +16,18 @@ const parse = async (markdown: string, css: string = '') => {
   const minifiedCss = css.split('\n').join('');
 
   const linkDict = {
-    highlightjs: `<link rel="stylesheet" href="https://unpkg.com/@highlightjs/cdn-assets@11.7.0/styles/atom-one-dark.min.css">`,
+    highlightjs: `<link rel="stylesheet" href="https://unpkg.com/@highlightjs/cdn-assets@11.7.0/styles/atom-one-dark.min.css"/>`,
     sanitize: `<link rel="stylesheet" href="https://unpkg.com/sanitize.css"/>`,
+    katex: `<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.16.4/dist/katex.min.css"/>`,
   } as const;
   const links = Object.values(linkDict).join('');
 
   return await unified()
     .use(remarkParse)
     .use(remarkGfm)
+    .use(remarkMath)
     .use(remarkRehype, { allowDangerousHtml: true })
+    .use(rehypeKatex)
     .use(rehypeRaw)
     .use(rehypeHighlight, { languages })
     .use(rehypeMinifyWhitespace)
